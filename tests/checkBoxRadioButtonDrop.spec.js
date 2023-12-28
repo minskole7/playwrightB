19fc42d895b0632766019a7c9673204d95b61aa0
minskole7 <minskole7@gmail.com>
2023-12-28 19:49:11 +0530

    added

--- tests/checkBoxRadioButtonDrop.spec.js
+++ tests/checkBoxRadioButtonDrop.spec.js
@@ -4,17 +4,7 @@ const assert = require ('assert');
 test("Drop down",async({page})=>{
     // arrangement
     await page.goto('https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html')
-    //actions
-    await page.selectOption('#dropdowm-menu-1','Python')
-    // assetion
-    const actualValue = await page.$eval("#dropdowm-menu-1",(el)=>el.value);
-    assert.strictEqual(actualValue,"python")
-    await page.selectOption('#dropdowm-menu-2','Maven')
-    const actualValue2 = await page.$eval("#dropdowm-menu-2",(el)=>el.value);
-    assert.strictEqual(actualValue2,"maven")
-    //<h1>Dropdown Menu(s), Checkboxe(s) &amp; Radio Button(s)</h1>
-    let textContentVal= await page.$eval('h1',(el)=>el.textContent())
-    assert.strictEqual(textContentVal,"Dropdown Menu(s), Checkboxe(s) &amp; Radio Button(s)")
+   
 })
 
 
--- tests/traverse.spec.js
+++ tests/traverse.spec.js
@@ -1,22 +1,22 @@
 
 const { test, expect } = require('@playwright/test');
-const assert = require ('assert');
-test("children elements",async({page})=>{
+const assert = require('assert');
+test("children elements", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let childrens = await page.$$('.traversal-buttons > *')
     expect(childrens.length).toBe(5)
-   
+
 })
 
-test("firstChild",async({page})=>{
+test("firstChild", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let firstText = await page.locator('.traversal-drinks-list > li').first().innerText();
     expect(firstText).toBe("Coffee")
 })
 
-test("lastChild",async({page})=>{
+test("lastChild", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let lastText = await page.locator('.traversal-drinks-list > li').last().innerText();
@@ -24,7 +24,7 @@ test("lastChild",async({page})=>{
 
 })
 
-test("nth:child",async({page})=>{
+test("nth:child", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let lastText = await page.locator('.traversal-drinks-list > li').nth(1).innerText()
@@ -32,11 +32,11 @@ test("nth:child",async({page})=>{
 
 })
 
-test.only("prevElement",async({page})=>{
+test("prevElement", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let milkElement = await page.$('#milk')
-    let txtFound = await milkElement.evaluate((el)=>{
+    let txtFound = await milkElement.evaluate((el) => {
         let text = el.previousElementSibling.textContent
         return text
     });
@@ -44,11 +44,11 @@ test.only("prevElement",async({page})=>{
 
 })
 
-test.only("nextElement",async({page})=>{
+test("nextElement", async ({ page }) => {
     // arrangement
     await page.goto('https://webdriveruniversity.com/Data-Table/index.html')
     let milkElement = await page.$('#milk')
-    let txtFound = await milkElement.evaluate((el)=>{
+    let txtFound = await milkElement.evaluate((el) => {
         let text = el.nextElementSibling.textContent
         return text
     });
@@ -57,3 +57,123 @@ test.only("nextElement",async({page})=>{
 })
 
 
+// children() // nth() // first() // last() // evaluate()
+// js using evalute
+// remaining topic to be covered AJAX
+
+
+
+
+// test('Filter Elements Based on Text Content with CSS Selector', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+//     let buttons = await page.$$('button')
+//     for(const button of buttons){
+//         let linkText = await button.textContent()
+//         console.log(linkText)
+//     }
+
+// });
+
+
+// test('Find and Print Text of Multiple Elements', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+
+//     // Use page.locator with a selector to find multiple elements
+//     const elements = await page.locator('button');
+
+//     // Get the count of found elements
+//     const count = await elements.count();
+//     console.log('Number of elements found:', count);
+
+//     // Iterate through the found elements and print their text content
+//     for (let i = 0; i < count; i++) {
+//         const textContent = await elements.nth(i).innerText();
+//         console.log('Element Text:', textContent);
+//     }
+// });
+
+// test.only('Find and Print Text of Multiple Elements with Flag', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+
+//     // Use page.locator with a selector to find multiple elements
+//     const elements = await page.locator('button');
+
+//     // Get the count of found elements
+//     const count = await elements.count();
+//     console.log('Number of elements found:', count);
+
+//     // Flag to check if the button with text "Button-2" is found
+//     let button2Found = false;
+
+//     // Iterate through the found elements and print their text content
+//     for (let i = 0; i < count; i++) {
+//         const textContent = await elements.nth(i).innerText();
+//         console.log('Element Text:', textContent);
+
+//         // Check if a button with the text "Button-2" is found
+//         if (textContent === 'Button-2') {
+//             console.log('Button-2 found!');
+//             button2Found = true;
+//             break; // Break out of the loop if the button is found
+//         }
+//     }
+
+//     // Add an assertion based on the flag
+//     await expect(button2Found).toBe(true);
+// });
+
+// test.only('Count Buttons Starting with Text "Button"', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+//     // Use page.$$ to find all buttons with the specified selector
+//     const buttons = await page.$$('button');
+//     // Use the filter method to get buttons starting with the text "Button"
+//     const filteredButtons = buttons.filter(async (button) => {
+//         const buttonText = await button.textContent();
+//         return buttonText.startsWith('Button');
+//     });
+//     // Log the count of buttons starting with the text "Button"
+//     console.log('Number of Buttons Starting with "Button":', filteredButtons.length);
+//     // Add an assertion to check if there are at least 2 such buttons
+//     await expect(filteredButtons.length).toBeGreaterThan(1); // Adjust the expectation as needed
+// });
+
+// test.only('Count Buttons Starting with Text "Button"', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+//     // Use page.$$ to find all buttons with the specified selector
+//     const buttons = await page.$$('button');
+//     // Use the filter method to get buttons starting with the text "Button"
+//     const filteredButtons = await Promise.all(
+//         buttons.map(async (button) => {
+//             // const buttonText = await button.innerText();
+//             // return buttonText.startsWith('Button');
+//             return button.innerText().includes('Button')
+//         })
+//     );
+
+//     // Count the number of buttons starting with the text "Button"
+//     const count = filteredButtons.filter((result) => result).length;
+
+//     // Log the count of buttons starting with the text "Button"
+//     console.log('Number of Buttons Starting with "Button":', count);
+
+//     // Add an assertion to check if there are at least 2 such buttons
+//     await expect(count).toBeGreaterThan(1); // Adjust the expectation as needed
+// });
+
+
+// test.only('Count Buttons Starting with Text "Button"', async ({ page }) => {
+//     await page.goto('https://webdriveruniversity.com/Data-Table/index.html');
+//     // Use page.$$ to find all buttons with the specified selector
+//     const buttons = await page.$$('button');
+//     // Use the filter method to get buttons starting with the text "Button"
+//     const filteredButtons = buttons.filter(async (button) => {
+//         const buttonText = await button.innerText();
+//         return buttonText.startsWith('Button');
+//     });
+
+//     // Log the count of buttons starting with the text "Button"
+//     console.log('Number of Buttons Starting with "Button":', filteredButtons.length);
+
+//     // Add an assertion to check if there are at least 2 such buttons
+//     await expect(filteredButtons.length).toBeGreaterThan(1); // Adjust the expectation as needed
+// });
